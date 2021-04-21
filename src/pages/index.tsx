922dac07720f151cfb273e10c445afa8c00d5c7a
Matheus Aguiar <first_mf@hotmail.com>
2021-04-21 16:25:01 -0300

    add Preview Mode

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import { useState } from 'react';
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import Link from 'next/link';
 import Prismic from '@prismicio/client';
 import { AiOutlineCalendar, AiOutlineUser } from 'react-icons/ai';
-import { format, parseISO } from 'date-fns';
+import { format } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 
 import Header from '../components/Header';
@@ -31,45 +33,53 @@ interface PostPagination {
 
 interface HomeProps {
   postsPagination: PostPagination;
+  preview: boolean;
 }
 
-export default function Home({ postsPagination: { next_page, results } }:HomeProps) {
-  const [posts, setPosts] = useState(results.map(post => {
-    return {
-      ...post,
-      first_publication_date: format(parseISO(post.first_publication_date),
-      'dd MMM yyyy',
-      { locale: ptBR }
-      )
-    }
-  }))
+export default function Home({
+  postsPagination: { next_page, results },
+  preview,
+}: HomeProps) {
+  const [posts, setPosts] = useState(
+    results.map(post => {
+      return {
+        ...post,
+        first_publication_date: format(
+          new Date(post.first_publication_date),
+          'dd MMM yyyy',
+          { locale: ptBR }
+        ),
+      };
+    })
+  );
 
-  const [nextPage, setNextPage] = useState(next_page)
+  const [nextPage, setNextPage] = useState(next_page);
 
   function handleLoadMore() {
     fetch(next_page)
-    .then(response => response.json())
-    .then(response => {
-      const newPosts = response.results.map(post => {
-        return {
-          uid: post.uid,
-          first_publication_date: format(parseISO(post.first_publication_date),
-          'dd MMM yyyy',
-          { locale: ptBR }
-          ),
-          data: {
-           title: post.data.title,
-           subtitle: post.data.subtitle,
-           author: post.data.author,
-          },
-        }
+      .then(response => response.json())
+      .then(response => {
+        const newPosts = response.results.map(post => {
+          return {
+            uid: post.uid,
+            first_publication_date: format(
+              new Date(post.first_publication_date),
+              'dd MMM yyyy',
+              { locale: ptBR }
+            ),
+            data: {
+              title: post.data.title,
+              subtitle: post.data.subtitle,
+              author: post.data.author,
+            },
+          };
+        });
+        setPosts([...posts, ...newPosts]);
+        setNextPage(response.next_page);
       })
-      setPosts([...posts, ...newPosts]);
-      setNextPage(response.next_page);
-    })
-    .catch(err => {
-      console.error('Failed retrieving information', err);
-    });
+      .catch(err => {
+        console.error('Failed retrieving information', err);
+      });
   }
 
   return (
@@ -80,70 +90,79 @@ export default function Home({ postsPagination: { next_page, results } }:HomePro
       <Header />
 
       <main className={commonStyles.container}>
-        { posts.map(post => (
-            <Link key={post?.uid}  href={`/post/${post?.uid}`}>
-              <a className={styles.teste}>
-                <div className={styles.posts}>
-                  <strong>{post?.data?.title}</strong>
-                  <span>{post?.data?.subtitle}</span>
-                  <ul>
-                    <li>
-                      <AiOutlineCalendar />
-                      {post.first_publication_date}
-                    </li>
-                    <li style={{textTransform: 'capitalize'}}>
-                      <AiOutlineUser />
-                      {post?.data?.author}
-                    </li>
-                  </ul>
-                </div>
-              </a>
+        {posts.map(post => (
+          <Link key={post?.uid} href={`/post/${post?.uid}`}>
+            <a className={styles.teste}>
+              <div className={styles.posts}>
+                <strong>{post?.data?.title}</strong>
+                <span>{post?.data?.subtitle}</span>
+                <ul>
+                  <li>
+                    <AiOutlineCalendar />
+                    {post.first_publication_date}
+                  </li>
+                  <li style={{ textTransform: 'capitalize' }}>
+                    <AiOutlineUser />
+                    {post?.data?.author}
+                  </li>
+                </ul>
+              </div>
+            </a>
+          </Link>
+        ))}
+        {nextPage && (
+          <button
+            className={styles.loadmore}
+            type="button"
+            onClick={handleLoadMore}
+          >
+            Carregar mais posts
+          </button>
+        )}
+        {preview && (
+          <aside className={commonStyles.exitPreview}>
+            <Link href="/api/exit-preview">
+              <a>Sair do modo Preview</a>
             </Link>
-          ))
-        }
-         {
-          nextPage && (
-            <button className={styles.loadmore} type="button" onClick={handleLoadMore}>
-              Carregar mais posts
-            </button>
-          )
-        }
+          </aside>
+        )}
       </main>
     </>
   );
 }
 
- export const getStaticProps: GetStaticProps = async () => {
-    const prismic = getPrismicClient();
+export const getStaticProps: GetStaticProps = async ({ preview = false }) => {
+  const prismic = getPrismicClient();
 
-    const postsResponse = await prismic.query([
-      Prismic.Predicates.at('document.type', 'posts')
-    ],
+  const postsResponse = await prismic.query(
+    [Prismic.Predicates.at('document.type', 'posts')],
     {
       fetch: ['posts.title', 'posts.subtitle', 'posts.author'],
-      pageSize: 1
-    });
-
-    const posts = postsResponse.results.map(post => {
-      return {
-        uid: post.uid,
-        first_publication_date: post.first_publication_date,
-        data: {
-         title: post.data.title,
-         subtitle: post.data.subtitle,
-         author: post.data.author,
-        },
-      }
-    })
-
-    const postsPagination = {
-      next_page: postsResponse.next_page,
-      results: posts,
+      pageSize: 1,
     }
+  );
 
+  const posts = postsResponse.results.map(post => {
     return {
-      props: {
-        postsPagination
-      }
-  }
- };
+      uid: post.uid,
+      first_publication_date: post.first_publication_date,
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author,
+      },
+    };
+  });
+
+  const postsPagination = {
+    next_page: postsResponse.next_page,
+    results: posts,
+  };
+
+  return {
+    props: {
+      postsPagination,
+      preview,
+    },
+  };
+};
--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,10 +1,19 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-return-assign */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import { GetStaticPaths, GetStaticProps } from 'next';
 import Head from 'next/head';
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { RichText } from 'prismic-dom';
-import { AiOutlineCalendar, AiOutlineUser, AiOutlineClockCircle } from 'react-icons/ai';
+import {
+  AiOutlineCalendar,
+  AiOutlineUser,
+  AiOutlineClockCircle,
+} from 'react-icons/ai';
 import Prismic from '@prismicio/client';
-import { parseISO, format } from 'date-fns';
+import { format } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 
 import Header from '../../components/Header';
@@ -12,6 +21,7 @@ import Header from '../../components/Header';
 import { getPrismicClient } from '../../services/prismic';
 
 import styles from './post.module.scss';
+import commonStyles from '../../styles/common.module.scss';
 
 interface Post {
   first_publication_date: string | null;
@@ -33,128 +43,162 @@ interface Post {
 
 interface PostProps {
   post: Post;
+  preview: boolean;
 }
 
- export default function Post({ post }: PostProps) {
+export default function Post({ post, preview }: PostProps) {
   const router = useRouter();
 
   function countWords(str) {
-    str = str.replace(/(^\s*)|(\s*$)/gi,"");
-    str = str.replace(/[ ]{2,}/gi," ");
-    str = str.replace(/\n /,"\n");
+    str = str.replace(/(^\s*)|(\s*$)/gi, '');
+    str = str.replace(/[ ]{2,}/gi, ' ');
+    str = str.replace(/\n /, '\n');
     return str.split(' ').length;
-    }
-
-  const numberOfWords = post.data.content[0].body.reduce((acc, element) => {
-      const words = countWords(element.text);
-      return acc += words;
-  }, 0)
-
-  const timeToRead = Math.ceil(numberOfWords/200);
-
-  if (router.isFallback) {
-    return <div>Carregando...</div>
   }
 
-   return(
-     <>
-     <Header />
-     <Head>
-       <title>{post.data?.title} | Spacetraveling</title>
-     </Head>
+  const numberOfWords = post.data.content[0].body.reduce((acc, element) => {
+    const words = countWords(element.text);
+    return (acc += words);
+  }, 0);
+
+  const timeToRead = Math.ceil(numberOfWords / 200);
+
+  if (router.isFallback) {
+    return <div>Carregando...</div>;
+  }
+
+  return (
+    <>
+      <Header />
+      <Head>
+        <title>{post.data?.title} | Spacetraveling</title>
+      </Head>
       <main className={styles.container}>
         <img src={post.data?.banner?.url} alt="banner" />
         <div className={styles.content}>
-            <h1>{post.data?.title}</h1>
-            <div className={styles.infos}>
-                <div>
-                  <AiOutlineCalendar />
-                  {format(parseISO(post.first_publication_date),
-                    'dd MMM yyyy',
-                    { locale: ptBR }
-                    )
-                  }
-                </div>
-                <div style={{textTransform: 'capitalize'}}>
-                  <AiOutlineUser />
-                  {post.data?.author}
-                </div>
-                <div>
-                  <AiOutlineClockCircle />
-                  {timeToRead} min
-                </div>
+          <h1>{post.data?.title}</h1>
+          <div className={styles.infos}>
+            <div>
+              <AiOutlineCalendar />
+              {post.first_publication_date}
             </div>
+            <div style={{ textTransform: 'capitalize' }}>
+              <AiOutlineUser />
+              {post.data?.author}
+            </div>
+            <div>
+              <AiOutlineClockCircle />
+              {timeToRead} min
+            </div>
+          </div>
 
-            {post.data.content.map(content => {
-              return (
-                <article key={content.heading}>
-                  <h2>{content.heading}</h2>
-                  <div
-                      className={styles.postContent}
-                      dangerouslySetInnerHTML={{
-                        __html: RichText.asHtml(content.body)
-                    }}
-                  />
-                </article>
-              )
-            })}
+          {post.data.content.map(content => {
+            return (
+              <article key={content.heading}>
+                <h2>{content.heading}</h2>
+                <div
+                  className={styles.postContent}
+                  dangerouslySetInnerHTML={{
+                    __html: RichText.asHtml(content.body),
+                  }}
+                />
+              </article>
+            );
+          })}
+          {!preview && (
+            <footer>
+              <hr />
+              <div className={styles.navigation}>
+                <div className={styles.navigationItem}>
+                  Post anterior
+                  <button type="button" onClick={() => {}}>
+                    Carregar mais posts
+                  </button>
+                </div>
+                <div className={styles.navigationItem}>
+                  TESTE dfsdf
+                  <button type="button" onClick={() => {}}>
+                    Próximo post
+                  </button>
+                </div>
+              </div>
+            </footer>
+          )}
+          {preview && (
+            <aside className={commonStyles.exitPreview}>
+              <Link href="/api/exit-preview">
+                <a>Sair do modo Preview</a>
+              </Link>
+            </aside>
+          )}
         </div>
       </main>
-     </>
-   )
- }
+    </>
+  );
+}
 
- export const getStaticPaths: GetStaticPaths = async () => {
-   const prismic = getPrismicClient();
+export const getStaticPaths: GetStaticPaths = async () => {
+  const prismic = getPrismicClient();
 
-   const posts = await prismic.query([
-     Prismic.Predicates.at('document.type','posts')
-   ]);
+  const posts = await prismic.query([
+    Prismic.Predicates.at('document.type', 'posts'),
+  ]);
 
-   const paths = posts.results.map(post => {
-     return {
-       params: {
-         slug: post.uid
-       }
-     }
-   })
-
+  const paths = posts.results.map(post => {
     return {
-      paths,
-      fallback: true
-  }
+      params: {
+        slug: post.uid,
+      },
+    };
+  });
 
- };
+  return {
+    paths,
+    fallback: true,
+  };
+};
 
- export const getStaticProps: GetStaticProps = async ({ params }) => {
-   const { slug } = params;
+export const getStaticProps: GetStaticProps = async ({
+  params,
+  preview = false,
+  previewData,
+}) => {
+  const { slug } = params;
 
-   const prismic = getPrismicClient();
-   const response = await prismic.getByUID('posts', String(slug), {});
+  const prismic = getPrismicClient();
+  const response = await prismic.getByUID('posts', String(slug), {
+    ref: previewData?.ref || null,
+  });
 
-    const post = {
-      uid: response.uid,
-      first_publication_date: response.first_publication_date,
-      data: {
+  const post = {
+    uid: response.uid,
+    first_publication_date: format(
+      new Date(response?.first_publication_date),
+      'dd MMM yyyy',
+      {
+        locale: ptBR,
+      }
+    ),
+    data: {
       banner: {
-      url: response.data.banner.url,
+        url: response.data.banner.url,
       },
       title: response.data.title,
       subtitle: response.data.subtitle,
       author: response.data.author,
-      content: response.data.content.map(content=> {
+      content: response.data.content.map(content => {
         return {
-        heading: content.heading,
-        body: [...content.body]
-        }
-      })
-      }
-    }
+          heading: content.heading,
+          body: [...content.body],
+        };
+      }),
+    },
+  };
 
-    return {
-      props: {
-        post
-      }
-    }
- };
-
+  return {
+    props: {
+      post,
+      preview,
+    },
+  };
+};
